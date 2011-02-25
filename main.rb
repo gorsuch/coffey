ec956112f3b6b2d2e31397facb9a8a1972606b58
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-02-24 21:48:42 -0600

    you can make posts.  woo hoo.

--- main.rb
+++ main.rb
@@ -1,13 +1,33 @@
 require 'sinatra'
 require 'sequel'
 
+$LOAD_PATH.unshift(File.dirname(__FILE__) + '/lib')
+
 configure do
+  set :path_root, '/'
+  set :path_posts_new, '/posts/new'
+  set :path_posts_create, '/posts/create'
+  
   set :database, Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://coffey.db')    
 end
 
-$LOAD_PATH.unshift(File.dirname(__FILE__) + '/lib')
 require 'post'
 
-get '/' do
-  'hi'
+get settings.path_root do
+  @posts = Post.reverse_order(:created_at)
+  erb :index
+end
+
+get settings.path_posts_new do
+  erb :edit, :locals => { :post => Post.new, :url => settings.path_posts_create }
+end
+
+post settings.path_posts_create do
+  post = Post.new(
+    :title => params[:title],
+    :body => params[:body],
+    :created_at => DateTime.now
+  )
+  post.save
+  redirect settings.path_root
 end
